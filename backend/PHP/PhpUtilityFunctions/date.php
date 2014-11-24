e4dc978a88abad1e91c8fba70eb3afe1aa435e23
JingwenTian <mini.mosquitor@gmail.com>
2014-11-24 19:58:32 +0800

    Update date.php

--- backend/PHP/PhpUtilityFunctions/date.php
+++ backend/PHP/PhpUtilityFunctions/date.php
@@ -114,5 +114,39 @@ CREATE TABLE IF NOT EXISTS `dede_tongji` (
 */
 
 
+//---------------------------------------------------------------------------------------------------
+
+//php获取今日开始时间戳和结束时间戳
+ 
+$beginToday=mktime(0,0,0,date('m'),date('d'),date('Y'));
+ 
+$endToday=mktime(0,0,0,date('m'),date('d')+1,date('Y'))-1;
+ 
+//php获取昨日起始时间戳和结束时间戳
+ 
+$beginYesterday=mktime(0,0,0,date('m'),date('d')-1,date('Y'));
+ 
+$endYesterday=mktime(0,0,0,date('m'),date('d'),date('Y'))-1;
+ 
+//php获取上周起始时间戳和结束时间戳
+ 
+$beginLastweek=mktime(0,0,0,date('m'),date('d')-date('w')+1-7,date('Y'));
+ 
+$endLastweek=mktime(23,59,59,date('m'),date('d')-date('w')+7-7,date('Y'));
+ 
+//php获取本月起始时间戳和结束时间戳
+ 
+$beginThismonth=mktime(0,0,0,date('m'),1,date('Y'));
+ 
+$endThismonth=mktime(23,59,59,date('m'),date('t'),date('Y'));
+
+
+//---------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 ?>
