9e6566e6fbfd9874394df9ae037f75a2f0bc2570
JingwenTian <mini.mosquitor@gmail.com>
2015-01-21 00:06:58 +0800

    写入与读取标签分隔文件(CSV等)

--- backend/PHP/snippetLibrary/write_read_tabbed_file.php
+++ backend/PHP/snippetLibrary/write_read_tabbed_file.php
@@ -3,27 +3,26 @@
 //Two simple functions to read an write tab seperated files (like CSV files, etc).
 //(BTW: The files are good readable for humans ;-)
 
-//
+
 // save an array as tab seperated text file
-//
- 
+
 function write_tabbed_file($filepath, $array, $save_keys=false){
     $content = '';
- 
+
     reset($array);
     while(list($key, $val) = each($array)){
- 
+
         // replace tabs in keys and values to [space]
         $key = str_replace("\t", " ", $key);
         $val = str_replace("\t", " ", $val);
- 
+
         if ($save_keys){ $content .=  $key."\t"; }
- 
+
         // create line:
         $content .= (is_array($val)) ? implode("\t", $val) : $val;
         $content .= "\n";
     }
- 
+
     if (file_exists($filepath) && !is_writeable($filepath)){ 
         return false;
     }
@@ -34,16 +33,16 @@ function write_tabbed_file($filepath, $array, $save_keys=false){
     else { return false; }
     return true;
 }
- 
+
 //
 // load a tab seperated text file as array
 //
 function load_tabbed_file($filepath, $load_keys=false){
     $array = array();
- 
+
     if (!file_exists($filepath)){ return $array; }
     $content = file($filepath);
- 
+
     for ($x=0; $x < count($content); $x++){
         if (trim($content[$x]) != ''){
             $line = explode("\t", trim($content[$x]));
@@ -56,11 +55,11 @@ function load_tabbed_file($filepath, $load_keys=false){
     }
     return $array;
 }
- 
+
 /*
 ** Example usage:
 */
- 
+
 $array = array(
     'line1'  => array('data-1-1', 'data-1-2', 'data-1-3'),
     'line2' => array('data-2-1', 'data-2-2', 'data-2-3'),
@@ -68,10 +67,10 @@ $array = array(
     'line4' => 'foobar',
     'line5' => 'hello world'
 );
- 
+
 // save the array to the data.txt file:
 write_tabbed_file('data.txt', $array, true);
- 
+
 /* the data.txt content looks like this:
 line1	data-1-1	data-1-2	data-1-3
 line2	data-2-1	data-2-2	data-2-3
@@ -79,9 +78,9 @@ line3	data-3-1	data-3-2	data-3-3
 line4	foobar
 line5	hello world
 */
- 
+
 // load the saved array:
 $reloaded_array = load_tabbed_file('data.txt',true);
- 
+
 print_r($reloaded_array);
 // returns the array from above
