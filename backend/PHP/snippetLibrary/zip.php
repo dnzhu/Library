e0b41c418a0188296f224ad96980b3b633789b13
JingwenTian <mini.mosquitor@gmail.com>
2015-02-01 23:20:56 +0800

    PHP压缩和解压缩文件

--- backend/PHP/snippetLibrary/zip.php
+++ backend/PHP/snippetLibrary/zip.php
@@ -1,6 +1,8 @@
 <?php
 
-/* @创建一个zip文件 */
+/**
+ * PHP压缩文件 
+ */
 
 function create_zip($files = array(),$destination = '',$overwrite = false) {
     if(file_exists($destination) && !$overwrite){ //检测zip文件是否存在
@@ -29,6 +31,26 @@ function create_zip($files = array(),$destination = '',$overwrite = false) {
     }
 }
 
+//example 1
 $filename = $dbname . date('Ymd') . ".sql";
 $zipfilename=$dbname . date('Ymd') . ".zip";
 create_zip(array($filename),$zipfilename,true);//执行压缩文件
+//example 2
+create_zip(array('1.sql','2.sql'), '1.zip', true);//执行压缩文件
+
+/**
+ * PHP解压文件
+ */
+function unzip_file($file, $destination){
+    $zip = new ZipArchive() ;
+    //打开压缩文件
+    if ($zip->open($file) !== TRUE) {
+        die ('Could not open archive');
+    }
+    //创建文件
+    $zip->extractTo($destination);
+    $zip->close();
+    echo '成功';
+}
+
+unzip_file("a.zip","b");
