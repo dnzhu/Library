a7228051296c6834cf8ec30c5b76656f5a9616a7
JingwenTian <mini.mosquitor@gmail.com>
2015-02-10 16:44:44 +0800

    获取可读的文件大小

--- backend/PHP/snippetLibrary/Human_readable_filesize.php
+++ backend/PHP/snippetLibrary/Human_readable_filesize.php
@@ -20,3 +20,30 @@ function HumanReadableFilesize($size) {
 }
 
 print HumanReadableFilesize(filesize('test_2mb.zip'));
+
+//---------------------------------------------------------------------------------------------
+
+//A easy way to check the file size
+
+function StringSizeToBytes($Size){
+ 
+    $Unit = strtolower($Size);
+    $Unit = preg_replace('/[^a-z]/', '', $Unit);
+ 
+    $Value = intval(preg_replace('/[^0-9]/', '', $Size));
+ 
+    $Units = array('b'=>0, 'kb'=>1, 'mb'=>2, 'gb'=>3, 'tb'=>4);
+    $Exponent = isset($Units[$Unit]) ? $Units[$Unit] : 0;
+ 
+    return ($Value * pow(1024, $Exponent));            
+}
+
+// Example usage:
+// Check if a file is bigger than 10 MB
+ 
+if (filesize('example.zip') > StringSizeToBytes('10 MB')){
+    print 'File is to big !';
+}
+else {
+    print 'File is okay';
+}
