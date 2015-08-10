3d25b4288ac350af2d0d9d95080a2543257481e0
JingwenTian <mini.mosquitor@gmail.com>
2015-08-10 15:08:39 +0800

    Update date.php

--- backend/PHP/snippetLibrary/date.php
+++ backend/PHP/snippetLibrary/date.php
@@ -35,3 +35,35 @@ w - 数字型的星期几，如: "0" (星期日) 至 "6" (星期六)
 a - "am" 或是 "pm"
 A - "AM" 或是 "PM"
 */
+
+----
+
+echo date("Ymd",strtotime("now")), "\n"; 
+echo date("Ymd",strtotime("-1 week Monday")), "\n"; 
+echo date("Ymd",strtotime("-1 week Sunday")), "\n"; 
+echo date("Ymd",strtotime("+0 week Monday")), "\n"; 
+echo date("Ymd",strtotime("+0 week Sunday")), "\n"; 
+
+
+//date('n') 第几个月 
+//date("w") 本周周几 
+//date("t") 本月天数 
+
+echo '<br>上周:<br>'; 
+echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1-7,date("Y"))),"\n"; 
+echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-date("w")+7-7,date("Y"))),"\n"; 
+echo '<br>本周:<br>'; 
+echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),date("d")-date("w")+1,date("Y"))),"\n"; 
+echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-date("w")+7,date("Y"))),"\n"; 
+
+echo '<br>上月:<br>'; 
+echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m")-1,1,date("Y"))),"\n"; 
+echo date("Y-m-d H:i:s",mktime(23,59,59,date("m") ,0,date("Y"))),"\n"; 
+echo '<br>本月:<br>'; 
+echo date("Y-m-d H:i:s",mktime(0, 0 , 0,date("m"),1,date("Y"))),"\n"; 
+echo date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("t"),date("Y"))),"\n"; 
+
+$getMonthDays = date("t",mktime(0, 0 , 0,date('n')+(date('n')-1)%3,1,date("Y")));//本季度未最后一月天数 
+echo '<br>本季度:<br>'; 
+echo date('Y-m-d H:i:s', mktime(0, 0, 0,date('n')-(date('n')-1)%3,1,date('Y'))),"\n"; 
+echo date('Y-m-d H:i:s', mktime(23,59,59,date('n')+(date('n')-1)%3,$getMonthDays,date('Y'))),"\n"; 
